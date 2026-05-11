98b1c1341d24e445d412e2e8c80dd8894f20222e
Adam Ferreira <adam.ferreira@edu.ecole-89.com>
2026-05-11 12:31:40 +0200

    add Dispatch table

--- include/chat.h
+++ include/chat.h
@@ -20,13 +20,18 @@ struct chat_env {
     struct client *clients;
 };
 
+struct chat {
+    char *symbol;
+    void (*fptr)(struct chat_env *env, int i);
+};
+
 int stu_strcmp(const char *s1, const char *s2);
 int stu_strncmp(const char *s1, const char *s2, int n);
 int stu_atoi(const char *str);
 void init_config(struct config *settings);
 int parse_args(int ac, char **av, struct config *settings);
 int init_server(struct config *settings);
-
+int stu_strcmp_space(const char *s1, const char *s2);
 int init_env(struct chat_env *env, struct config *settings, int server_fd);
 void free_env(struct chat_env *env);
 void run_chat(struct chat_env *env);
@@ -34,5 +39,6 @@ void accept_client(struct chat_env *env);
 void disconnect_client(struct chat_env *env, int i);
 void handle_client(struct chat_env *env, int i);
 void broadcast_msg(struct chat_env *env, int sender_fd, char *msg, int len);
+//void nick (struct chat_env *env, int i);
 
 #endif
